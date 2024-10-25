cbdcf4d843df4210f6d498ac0b6a9f1f74ac6c29
Anastasiya <stasyaf@gmail.com>
2024-10-25 18:05:53 +0300

    init commit

---
+++ src/main/java/com/demo/dropit_api_test/DropitApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.demo.dropit_api_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropitApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropitApiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/dropit_api_test/DropitApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.dropit_api_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropitApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
